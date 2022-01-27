624eb56d77533be3b7bc645a48031ac5aa9fb475
whimax07 <59751270+whimax07@users.noreply.github.com>
2022-01-27 21:33:09 +0000

    Added some new lines.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use crate::gen_csv::wrap_csv_body;
-
-
 mod gen_csv;
 
-
 fn main() {
-    let image = wrap_csv_body("");
-    print!("{}", image);
+    let image = gen_csv::wrap_csv_body("");
+    print!("\n");
+    println!("{}", image);
+    print!("\n");
 }
