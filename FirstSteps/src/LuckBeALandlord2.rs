7bc076ad8ea0b7986073813f22ba7a02f07279a1
whimax07 <59751270+whimax07@users.noreply.github.com>
2022-03-19 15:18:44 +0000

    Added the start of the maximum score calculator.
    Removed Symbol#next() and replaced it with Symbol#next_and_carry. The next board function is very similar to my first version of the solver.

--- FirstSteps/src/LuckBeALandlord2.rs
+++ FirstSteps/src/LuckBeALandlord2.rs
@@ -1,5 +1,5 @@
 // Define the symbols set.
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Symbol {
     pub name: &'static str,
     pub display_char: char,
@@ -13,14 +13,14 @@ pub struct Symbol {
 impl Symbol {
     pub const FIRST_SYMBOL: Symbol = Symbol::FLOWER;
 
-    pub const FLOWER: Symbol = Symbol {
+    const FLOWER: Symbol = Symbol {
         name: "Flower",
         display_char: 'F',
         base_value: 1,
         multi_map: flower_multipliers,
     };
 
-    pub const SUN: Symbol = Symbol {
+    const SUN: Symbol = Symbol {
         name: "Sun",
         display_char: 'S',
         base_value: 3,
@@ -28,14 +28,17 @@ impl Symbol {
     };
 
 
+    /// This function returns true if it carried aka loop around to the start of the symbol set.
+    ///
     /// Note(Max): This is the best way to get the next symbol I have so far managed. If I can add
-    /// an index and an array of the symbols I can index into the array with the index.
-    pub fn next(self) -> Option<Symbol> {
-        return match self {
-            Symbol::FLOWER => Some(Symbol::SUN),
-            Symbol::SUN=> None,
-            _ => panic!("Please use one of the predefined Symbols.")
+    // an index and an array of the symbols I can index into the array with the index.
+    pub fn next_and_carry(&mut self) -> bool {
+        match *self {
+            Symbol::FLOWER => *self = Symbol::SUN,
+            Symbol::SUN=> *self = Symbol::FIRST_SYMBOL,
+            _ => panic!("Please use with a game symbol, one of the predefined const symbols.")
         }
+        return *self == Symbol::FIRST_SYMBOL
     }
 }
 
@@ -51,9 +54,59 @@ fn sun_multipliers(_symbol: Symbol) -> f32 {
 }
 
 
+// Game code.
+struct GameState {
+    board: Board,
+    best_board: Board,
+    score: i128,
+    best_score: i128,
+}
+
+impl GameState {
+    const WIDTH: usize = 5;
+    const HEIGHT: usize = 4;
+
+    pub fn new() -> Self {
+        Self {
+            board: [[Symbol::FIRST_SYMBOL; GameState::WIDTH]; GameState::HEIGHT],
+            best_board: [[Symbol::FIRST_SYMBOL; GameState::WIDTH]; GameState::HEIGHT],
+            score: 0,
+            best_score: 0
+        }
+    }
+
+    pub fn next_board(&mut self) -> bool {
+        for row in self.board.iter_mut() {
+            for symbol in row.iter_mut() {
+                if !symbol.next_and_carry() { return true }
+            }
+        }
+        return false
+    }
+}
+
+type Board = [[Symbol; GameState::WIDTH]; GameState::HEIGHT];
+
+
+fn run_solver() {
+    // init the game boards.
+    let mut game_state: GameState = GameState::new();
+    while GameState::next_board(&mut game_state) {
+        // Calculate the score of the board.
+    }
+
+    // Print the results.
+}
+
+
 // ...
 pub fn main() {
-    println!("{:?}", Symbol::FLOWER.next());
-    println!("{:?}", Symbol::SUN.next());
+    run_solver();
+    let mut x = Symbol::FLOWER;
+    x.next_and_carry();
+    println!("{:?}", x);
+    x = Symbol::SUN;
+    x.next_and_carry();
+    println!("{:?}", x);
 }
 
